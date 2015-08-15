fc3d858951132ef270edca472c4275aed8fb794c
dan <dan.j.thomson@googlemail.com>
2015-08-15 16:57:22 +1200

    First Commit

--- src/main/scala/ScalaPadding.scala
+++ src/main/scala/ScalaPadding.scala
@@ -1,6 +1,25 @@
 /**
  * Created by dan on 15/08/2015.
  */
-class ScalaPadding {
+object ScalaPadding {
+
+  def pad(str:String):String = str match {
+    case "" => "000000000000"
+    case null =>  "000000000000"
+    case _ => padIt(str.reverse)
+  }
+
+  def padIt(str:String):String = {
+
+    val (digitTail,start) = str.span(_.isDigit)
+    if(digitTail.length >= 12) str.reverse
+    else{
+      val paddingSize = 12 - digitTail.length
+      val padding = "0" * paddingSize
+      (digitTail + padding + start).reverse
+    }
+
+
+  }
 
 }
