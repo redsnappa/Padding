77ccb604bc1660b0c8c051cfc59e063ac6334a31
dan <dan.j.thomson@googlemail.com>
2015-08-15 17:44:28 +1200

    First Commit

--- src/main/java/JPadding.java
+++ src/main/java/JPadding.java
@@ -2,4 +2,42 @@
  * Created by dan on 15/08/2015.
  */
 public class JPadding {
+
+
+    public static String pad(String str){
+        if(str==null || "".equals(str)) return "000000000000";
+
+        int i = str.length();
+        String digits = "0123456789";
+        String digitTail = "";
+        while(i > 0){
+            String currentLetter = str.substring(i-1,i);
+            if(digits.contains(currentLetter)){
+                digitTail = currentLetter + digitTail;
+                --i;
+            }else{
+                break;
+            }
+        }
+        if(digitTail.length() >= 12) return str;
+        else{
+            String start = str.substring(0,i);
+            int paddingLength = 12 - digitTail.length();
+            String padding = createPadding("0",paddingLength);
+            return start + padding + digitTail;
+        }
+    }
+
+    public static String createPadding(String str, int repeatCount){
+        if(repeatCount <= 0) return str;
+
+        String result = "";
+        while(repeatCount > 0){
+            result = result + str;
+            --repeatCount;
+        }
+        return result;
+    }
+
+
 }
--- src/test/java/JavaPaddingTest.scala
+++ src/test/java/JavaPaddingTest.scala
@@ -4,58 +4,58 @@
 
 import org.scalatest.FlatSpec
 
-class ScalaPaddingTest extends FlatSpec {
+class JavaPaddingTest extends FlatSpec {
 
   "Given an empty string to pad" should "return \"000000000000\"" in {
-    assert(ScalaPadding.pad("") === "000000000000")
+    assert(JPadding.pad("") === "000000000000")
   }
 
   "Given an null to pad" should "return \"000000000000\"" in {
-    assert(ScalaPadding.pad(null) === "000000000000")
+    assert(JPadding.pad(null) === "000000000000")
   }
 
   "Given letters only \"abc\" to pad" should "return \"abc000000000000\"" in {
-    assert(ScalaPadding.pad("abc") === "abc000000000000")
+    assert(JPadding.pad("abc") === "abc000000000000")
   }
 
   "Given numbers only string under 12 in length \"123\" to pad" should "return \"000000000123\"" in {
-    assert(ScalaPadding.pad("123") === "000000000123")
+    assert(JPadding.pad("123") === "000000000123")
   }
 
   "Given mixes ending in letters \"123abc\" to pad" should "return \"123abc000000000000\"" in {
-    assert(ScalaPadding.pad("123abc") === "123abc000000000000")
+    assert(JPadding.pad("123abc") === "123abc000000000000")
   }
 
   "Given a full digit string with length over 12 \"0123456789102\" to pad" should "return \"0123456789102\"" in {
-    assert(ScalaPadding.pad("0123456789102") === "0123456789102")
+    assert(JPadding.pad("0123456789102") === "0123456789102")
   }
 
   "Given a full digit string with length of 12 \"012345678912\" to pad" should "return \"012345678912\"" in {
-    assert(ScalaPadding.pad("012345678912") === "012345678912")
+    assert(JPadding.pad("012345678912") === "012345678912")
   }
 
   "Given a full digit string with length of under 12 \"01234567891\" to pad" should "return \"001234567891\"" in {
-    assert(ScalaPadding.pad("01234567891") === "001234567891")
+    assert(JPadding.pad("01234567891") === "001234567891")
   }
 
   "Given a string with letters and a digit tail of length of  12 \"abc123456789101\" to pad" should "return \"abc123456789101\"" in {
-    assert(ScalaPadding.pad("abc123456789101") === "abc123456789101")
+    assert(JPadding.pad("abc123456789101") === "abc123456789101")
   }
 
   "Given a string with letters and a digit tail of length of over 12 \"abc1234567891011\" to pad" should "return \"abc1234567891011\"" in {
-    assert(ScalaPadding.pad("abc1234567891011") === "abc1234567891011")
+    assert(JPadding.pad("abc1234567891011") === "abc1234567891011")
   }
 
   "Given a string with letters and a digit tail of length of under 12 \"abc123\" to pad" should "return \"abc000000000123\"" in {
-    assert(ScalaPadding.pad("abc123") === "abc000000000123")
+    assert(JPadding.pad("abc123") === "abc000000000123")
   }
 
   "Given mixed ending in digits and string in digits [under 12 in length] \"123abc123\" to pad" should "return \"123abc000000000123\"" in {
-    assert(ScalaPadding.pad("123abc123") === "123abc000000000123")
+    assert(JPadding.pad("123abc123") === "123abc000000000123")
   }
 
   "Given \"000000000000\" to pad" should "return \"000000000000\"" in {
-    assert(ScalaPadding.pad("000000000000") === "000000000000")
+    assert(JPadding.pad("000000000000") === "000000000000")
   }
 
 }
